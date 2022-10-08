ea8b54c46ce619da264c176dacd44785f0553a83
Pdom <ahb0327@naver.com>
2022-10-08 21:54:26 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,57 @@
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    pollster::block_on(run());
+}
+
+async fn run() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => {
+            //if !state.input(event) {
+            match event {
+                WindowEvent::CloseRequested
+                | WindowEvent::KeyboardInput {
+                    input:
+                        KeyboardInput {
+                            state: ElementState::Pressed,
+                            virtual_keycode: Some(VirtualKeyCode::Escape),
+                            ..
+                        },
+                    ..
+                } => *control_flow = ControlFlow::Exit,
+                WindowEvent::Resized(physical_size) => {
+                    state.resize(*physical_size);
+                }
+                WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                    // new_inner_size is &&mut so we have to dereference it twice
+                    state.resize(**new_inner_size);
+                }
+                _ => {}
+            }
+            //}
+        }
+        Event::RedrawRequested(window_id) if window_id == window.id() => {
+            // state.update();
+            // match state.render() {
+            //     Ok(_) => {}
+            //     Err(wgpu::SurfaceError::Lost) => state.resize(state.size),
+            //     Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+            //     Err(e) => eprintln!("{:?}", e),
+            // }
+        }
+        Event::MainEventsCleared => {
+            window.request_redraw();
+        }
+        _ => {}
+    });
 }
